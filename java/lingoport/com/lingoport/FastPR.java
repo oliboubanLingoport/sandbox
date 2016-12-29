6c97038140b13940e1ae6353684298c1babf7064
Olivier Libouban <oliboubanLingoport@users.noreply.github.com>
2016-12-29 10:58:27 -0700

    Update FastPR.java

--- java/lingoport/com/lingoport/FastPR.java
+++ java/lingoport/com/lingoport/FastPR.java
@@ -3,6 +3,6 @@ package com.lingoport;
 public class FastPR {
 
 	public static void main(String[] args) {
-		String greeting = "Fast Pull Request Test" + args[0]; // $NON-NLS-L$
+		String greeting = "Fast Pull Request Test" + args[0]; 
 	}
 }
