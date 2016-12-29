47bd938207b65bbc97b6eb629106b7e384856ce7
Olivier Libouban <oliboubanLingoport@users.noreply.github.com>
2016-12-29 10:14:50 -0700

    Update FastPR.java

--- java/lingoport/com/lingoport/FastPR.java
+++ java/lingoport/com/lingoport/FastPR.java
@@ -3,6 +3,6 @@ package com.lingoport;
 public class FastPR {
 
 	public static void main(String[] args) {
-		String greeting = "Fast Pull Request Test" + args[0];
+		String greeting = "Fast Pull Request Test" + args[0]; // Is this detected?
 	}
 }
